ce9f61db9428046d7f1bebbbd383d11068f96c74
whymezi <128997288+whymezi@users.noreply.github.com>
2023-05-26 23:48:40 +0600

    Add files via upload

---
+++ DZ7_zarina_26-03/app.js
@@ -0,0 +1,20 @@
+function func() {
+    var result;
+    result = 'Мышь'
+        document.getElementById("result").innerHTML = result;
+    var result2;
+    result2 = 'Глаз'
+        document.getElementById("result2").innerHTML = result2;
+    var result3;
+    result3 = 'Уши'
+        document.getElementById("result3").innerHTML = result3;
+    var result4;
+    result4 = 'CTRL+C'
+        document.getElementById("result4").innerHTML = result4;
+    var result5;
+    result5 = 'Яблоко'
+        document.getElementById("result5").innerHTML = result5;
+    var result6;
+    result6 = '26'
+        document.getElementById("result6").innerHTML = result6;
+    }
